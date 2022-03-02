cec02e01c76d45ce54aeecf2893c851e7b0a86a6
spatlol <siddharth.reddy7479@gmail.com>
2022-03-02 01:24:33 -0800

    fetches repos from /repos endpoint

--- src/components/Results.tsx
+++ src/components/Results.tsx
@@ -1,29 +1,48 @@
-import { PageInfo } from '../siteContent';
-import LinkModule from './LinkModule'
-import "../styles/results.css"
+import { PageInfo } from "../siteContent";
+import LinkModule from "./LinkModule";
+import "../styles/results.css";
+import { useEffect, useState } from "react";
+import { textSpanIntersectsWithPosition } from "typescript";
 
 interface ResultsProps {
-    sites: [string, PageInfo[]][];
+  sites: [string, PageInfo[]][];
 }
 
 export default function Results(props: ResultsProps) {
-    const links = props.sites.map((site) => {
-        const [initiative, siteInfo] = site;
-        const initiativeSites = siteInfo.map((checkSite) => (
-            <LinkModule
-                committee={initiative}
-                url={checkSite.url}
-                key={checkSite.url}
-                description={checkSite.description}
-            />
-        ));
-        return (
-                <div key={initiative}>{initiativeSites}</div>
-        );
-    })
-    return (
-        <div id="results">
-            {links}
-        </div>
-    )
+  const [repos, setRepos] = useState({ topic: [] });
+
+  const getRepos = async () => {
+    const response = await fetch("/repos");
+    const data = await response.json();
+    setRepos(data);
+    return;
+  };
+
+  useEffect(() => {
+    getRepos();
+  }, []);
+
+  console.log(repos);
+
+//   const list = Object.keys(repos).map((topic) => {
+//     const websites = repos.topic.map((link) => (
+//       <LinkModule committee={topic} url={link} key={link} description={"TBD"} />
+//     ));
+//     return <div key={topic}>{websites}</div>;
+//   });
+
+  const links = props.sites.map((site) => {
+    const [initiative, siteInfo] = site;
+    const initiativeSites = siteInfo.map((checkSite) => (
+      <LinkModule
+        committee={initiative}
+        url={checkSite.url}
+        key={checkSite.url}
+        description={checkSite.description}
+      />
+    ));
+    return <div key={initiative}>{initiativeSites}</div>;
+  });
+
+  return <div id="results">{links}</div>;
 }
