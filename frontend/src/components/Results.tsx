9489ba96ce95feb4770a6c2c48bc5d729a3f2b80
spatlol <siddharth.reddy7479@gmail.com>
2022-03-06 18:42:51 -0800

    added headers for each column and buttons to sort by website, status etc

--- frontend/src/components/Results.tsx
+++ frontend/src/components/Results.tsx
@@ -1,14 +1,24 @@
 import { PageInfo } from "../siteContent";
 import LinkModule from "./LinkModule";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowDown, faArrowUp } from "@fortawesome/free-solid-svg-icons";
 import "../styles/results.css";
-import { useEffect, useState } from "react";
-import { url } from "inspector";
+import { useState } from "react";
 
 interface ResultsProps {
   repoList: [string, string[]][];
 }
 
+enum Sort {
+  Site = "site",
+  Committee = "committee",
+  Status = "status",
+}
+
 export default function Results(props: ResultsProps) {
+  const [currentSort, setCurrentSort] = useState(Sort.Site);
+  const [sortDown, setSortDown] = useState(true);
+
   const list = props.repoList.map((topic) => {
     const [committee, ...urls] = topic;
     const websites = urls[0].map((link) => (
@@ -22,5 +32,33 @@ export default function Results(props: ResultsProps) {
     return <div key={committee}>{websites}</div>;
   });
 
-  return <div id="results">{list}</div>;
+  return (
+    <div id="results">
+      <div id="labelsContainer">
+        <div></div>
+        {[Sort.Site, Sort.Committee, Sort.Status].map((sort) => (
+          <button
+            className="label"
+            onClick={() => {
+              if (currentSort === sort) setSortDown((sortDown) => !sortDown);
+              else {
+                setSortDown(true);
+                setCurrentSort(sort);
+              }
+            }}
+          >
+            {sort}
+            {currentSort === sort && (
+              <FontAwesomeIcon
+                className="arrow"
+                icon={sortDown ? faArrowDown : faArrowUp}
+              />
+            )}
+          </button>
+        ))}
+      </div>
+
+      {list}
+    </div>
+  );
 }
